809373ccd73d5bf9bd853abeb2dd17eb63e68126
Дмитрий Венде <vende.d@mail.ru>
2022-11-14 16:33:17 +0300

    Создал модель юзера

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField('email address', unique=True)
