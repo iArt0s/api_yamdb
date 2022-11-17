a76862429f9bb7521193d12e1babbd8b61435167
vende <vende.d@mail.ru>
2022-11-17 12:05:35 +0300

    fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from .views import (RegisterView, VerifyUserView, CategoryViewSet,
     GenreViewSet,
     TitleViewSet,
-    UserViewSet, SelfUserViewSet
+    UserViewSet,SelfUserViewSet
 )
 
 
@@ -12,12 +12,11 @@ router_v1.register('auth/signup', RegisterView, basename = 'signup')
 router_v1.register(r'genres', GenreViewSet, basename = 'genres')
 router_v1.register(r'categories', CategoryViewSet, basename = 'categories')
 router_v1.register(r'titles', TitleViewSet, basename = 'titles')
-router_v1.register('users',UserViewSet, basename = 'users')
-router_v1.register(r'me',SelfUserViewSet, basename = 'me')
-
 
 
 urlpatterns=[
     path('api/v1/', include(router_v1.urls)),
-    path('api/v1/auth/token/', VerifyUserView.as_view(), name='token')
+    path('api/v1/auth/token/', VerifyUserView.as_view(), name='token'),
+    path('api/v1/users/',UserViewSet.as_view({'get': 'list'}), name = 'users'),
+    path('api/v1/users/me/',SelfUserViewSet, name = 'me')
 ]
