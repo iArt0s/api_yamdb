53035c7303d083f6a5da9bd0d3749b684374fef9
Arthem O <oarthemlive@gmail.com>
2022-11-18 17:32:32 +0300

    🛠добавил в views.py вьюсеты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -145,3 +145,35 @@ class UserViewSet(viewsets.ModelViewSet):
         user = User.objects.get(username=username)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#ПРОБЛЕМА С IsAuthOrReadOnly
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Viewset для ревью."""
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthOrReadOnly,)
+
+    def get_queryset(self):
+        pk = self.kwargs.get('title_id')
+        get_object_or_404(Title, pk=pk)
+        return Review.objects.filter(title__pk=pk)
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs['title_id'])
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Viewset для комментариев."""
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthOrReadOnly,)
+
+    def get_queryset(self):
+        pk = self.kwargs.get('review_id')
+        get_object_or_404(Review, pk=pk)
+        return Comment.objects.filter(review__pk=pk)
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs['review_id'])
+        serializer.save(author=self.request.user, review=review)
